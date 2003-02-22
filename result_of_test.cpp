95d04d0f5a55ca23f23db9923df27301107e4011
Douglas Gregor <doug.gregor@gmail.com>
2003-02-22 21:38:38 +0000

    Implementation of result_of<> as we're discussing it for the Bind proposal
    [SVN r1037]

---
+++ result_of_test.cpp
@@ -0,0 +1,34 @@
+#include <boost/result_of.hpp>
+#include <utility>
+#include <boost/static_assert.hpp>
+#include <boost/type_traits/is_same.hpp>
+
+class int_result_type { typedef int result_type; };
+
+class int_result_of
+{
+  template<typename F> struct result_of { typedef int type; };
+};
+
+class int_result_type_and_float_result_of
+{
+  typedef int result_type;
+  template<typename F> struct result_of { typedef float type; };
+};
+
+struct X {};
+
+int main()
+{
+  using namespace boost;
+
+  typedef int (*func_ptr)(float, double);
+  typedef int (X::*mem_func_ptr)(float);
+
+  BOOST_STATIC_ASSERT((is_same<result_of<int_result_type(float)>::type, int>::value));
+  BOOST_STATIC_ASSERT((is_same<result_of<int_result_of(double)>::type, int>::value));
+  BOOST_STATIC_ASSERT((is_same<result_of<int_result_type_and_float_result_of(char)>::type, int>::value));
+  BOOST_STATIC_ASSERT((is_same<result_of<func_ptr(char, float)>::type, int>::value));
+  BOOST_STATIC_ASSERT((is_same<result_of<mem_func_ptr(X,char)>::type, int>::value));
+  return 0;
+}
