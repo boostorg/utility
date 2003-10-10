45f35bd3b81a7706c6e3a7f9c7a1873ee2b99981
Jaakko Järvi <jarvi@cs.tamu.edu>
2003-10-10 16:50:48 +0000

    added a new test case to actually test enable_if_lazy :)
    [SVN r1661]

--- test/enable_if_lazy.cpp
+++ test/enable_if_lazy.cpp
@@ -62,12 +62,14 @@ typename lazy_enable_if_c<
 	  mult_traits<T, U> >::type
 mult(const T& x, const U& y) {return x * y;}
 
+double mult(int i, double d) { return (double)i * d; }
 
 int test_main(int, char*[])
 {
  
   BOOST_TEST(mult(1, 2) == 2);
   BOOST_TEST(mult(1.0, 3.0) == 3.0);
+  BOOST_TEST(mult(1, 3.0) == 3.0);
 
   return 0;
 }
