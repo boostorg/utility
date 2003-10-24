9e8c9b92cb478f278c1e91ed8916432fa43cbef4
Daniel Wallin <daniel@boostpro.com>
2003-10-24 09:54:09 +0000

    changed copyright
    [SVN r1703]

--- test/named_params_test.cpp
+++ test/named_params_test.cpp
@@ -1,6 +1,7 @@
-// Copyright Daniel Wallin 2003. Use, modification and distribution is
-// subject to the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE-1.0 or http://www.boost.org/LICENSE-1.0)
+// Copyright David Abrahams, Daniel Wallin 2003. Use, modification and 
+// distribution is subject to the Boost Software License, Version 1.0. 
+// (See accompanying file LICENSE_1_0.txt or copy at 
+// http://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/named_params.hpp>
 #include <boost/type_traits/is_convertible.hpp>
