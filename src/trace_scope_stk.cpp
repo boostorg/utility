c346d99b84f21d24916f02b7a18caaa43e922ccb
Larry Joe Evans <cppljevans@suddenlink.net>
2005-06-14 13:31:37 +0000

    Adding Jamfile.v2 needed by libs/policy_ptr/test/Jamfile.v2
    [SVN r2630]

---
+++ src/trace_scope_stk.cpp
@@ -0,0 +1,12 @@
+#include "boost/utility/trace_scope_stk.hpp"
+//        static
+      std::vector<std::string>
+utility::trace_scope_stk::
+    c_scope_names
+      ;
+//        static
+      std::string const
+utility::trace_scope_stk::
+    c_test_results[2]
+      = {"failed", "passed"}
+      ;
